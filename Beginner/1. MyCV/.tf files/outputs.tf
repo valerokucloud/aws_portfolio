422168c43b4e4f3311712f6cf4ce06687b28b734
Antonio Valero <valeroku.projects@gmail.com>
2025-09-21 13:00:32 +0200

    Delete Beginner/1. MyCV/.tf files directory

--- Beginner/1. MyCV/.tf files/outputs.tf
+++
@@ -1,8 +0,0 @@
-// Output definition:
-    output "s3_bucket_name" {
-    value = aws_s3_bucket.valeroku_mycv.bucket
-    }
-
-    output "aws_cloudfront_domain" {
-      value = aws_cloudfront_distribution.s3_distribution.domain_name
-    }
