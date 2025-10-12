b9d82aabaa8bded86b4469c472298eb9b5f577aa
Antonio Valero <valeroku.projects@gmail.com>
2025-10-12 10:37:30 +0200

    Add files via upload

---
+++ Beginner/4. Creating an IAM user + security policies/.tf files/main.tf
@@ -0,0 +1,26 @@
+# Creating a new group (Administrators):
+    resource "aws_iam_group" "Administrators_group" {
+      name = "Administrators"
+      path = "/users/"
+    }
+
+# IAM user creation:
+    resource "aws_iam_user" "admin_user" {
+    name = "admin_user"
+    # Path in which to create the user:
+        path = "/"
+    #  When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices:
+        force_destroy = true
+    }
+
+# Access key for the user:
+    resource "aws_iam_access_key" "AK_user" {
+    user = aws_iam_user.admin_user.name
+    }
+
+# AdminAccess policy for the user:
+    resource "aws_iam_user_policy_attachment" "admin_access_policy" {
+    user       = aws_iam_user.admin_user.name
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+    }
+    
