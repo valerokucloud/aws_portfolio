b533290e45394cb481fe06ee5a4d7c0adfe760ca
Antonio Valero <a.valero.b89@gmail.com>
2026-02-12 09:55:16 +0100

    add

---
+++ Intermediate/3. Recipe Web Application/frontend base config/src/configs/aws-exports.ts
@@ -0,0 +1,7 @@
+export const amplifyConfig = {
+  aws_project_region: '[CognitoRegion]',
+  aws_cognito_region: '[CognitoRegion]',
+  aws_user_pools_id: '[UserPoolId]',
+  aws_user_pools_web_client_id: '[UserPoolClientId]',
+  };
+
