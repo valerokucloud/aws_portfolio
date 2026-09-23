4fdd0dc121dc20637bd780964792012f24b9fe0d
Antonio Valero <a.valero.b89@gmail.com>
2026-09-23 11:26:22 +0200

    ec2 sg reference deletion

--- Intermediate/5. AWS Operations Platform/.tf files/ec2.tf
+++ Intermediate/5. AWS Operations Platform/.tf files/ec2.tf
@@ -14,8 +14,6 @@
       aws_subnet.public_b.id
     ], count.index)
 
-    vpc_security_group_ids = [aws_security_group.ec2.id]
-
     iam_instance_profile = aws_iam_instance_profile.ec2_ssm.name
 
     user_data = <<-EOF
